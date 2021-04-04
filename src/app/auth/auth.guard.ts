96904d18bc3aa3541326dbfcdb664d5f7c4249ae
lllouiseF <gailefamisan@gmail.com>
2021-04-04 16:08:13 +0800

    remove auth

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CanLoad,  UrlTree , UrlSegment, Route, Router} from '@angular/router';
-import { Observable, of } from 'rxjs';
-import { switchMap, take, tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
 
 @Injectable({
@@ -14,23 +13,10 @@ export class AuthGuard implements CanLoad {
     route: Route, 
     segments: UrlSegment[] ):
     Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-
-    return this.authService.userIsAuthenticated.pipe(
-      take(1), 
-      switchMap(isAuthenticated => {
-        if(!isAuthenticated) {
-          return this.authService.autoLogin();
-        } else {
-          return of(isAuthenticated);
-        }
-      }),
-      tap(isAuthenticated => {
-      if(!isAuthenticated){
-        this.router.navigateByUrl('/auth');
-     
-      }
-    })
-    );
+      if(!this.authService.userIsAuthenticated){
+      this.router.navigateByUrl('/auth');
+   
+    }
+    return this.authService.userIsAuthenticated;
 }
 }
-
